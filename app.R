ad6cab8b64b65bf4a2be3198db86c194baffcda1
VwakeM <vivek.nies@gmail.com>
2020-07-25 16:42:53 -0700

    Updates, bug fixes

--- app.R
+++ app.R
@@ -1,15 +1,17 @@
 library(shiny)
-library(tmap)
-library(tmaptools)
 library(tidyverse)
 library(shinythemes)
 library(plotly)
 library(leaflet)
 library(rgdal)
 
-record_type <- c("All years","Conflict", "Sighting")
-year_range <- c("All", "2000-2010", "2011-2019")
+states <- readOGR("Merge.shp")
 
+records1 <- read.csv("elephant_records.csv", header = T, stringsAsFactors = FALSE)
+
+record_type <- c("All", records1$Occurence_type)
+year_range <- c("All", "2000-2010", "2011-2019")
+  
 ui <- fluidPage(
   theme = shinytheme("cerulean"),
   titlePanel(title = "Elephant records"),
@@ -21,8 +23,8 @@ ui <- fluidPage(
       sidebarLayout(
         sidebarPanel(
           width = 3,
-          selectInput(inputId = "type", label = "Select type of record:", record_type),
-          selectInput(inputId = "years", label = "Select year:", year_range),
+          selectInput(inputId = "type", label = "Select type of incident:", record_type),
+          selectInput(inputId = "years", label = "Select year range:", year_range),
           submitButton(text = "Click to load/refresh map", icon = NULL, width = "100%"),
           br(),
           downloadButton("download_records", "Download data as CSV")
@@ -35,30 +37,55 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   records <- read.csv("elephant_records.csv", header = T, stringsAsFactors = FALSE)
-  points <- cbind(records$Long, records$Lat)
   
-  greenLeafIcon <- makeIcon(
+  ele_filter <- reactive({
+    
+    type <- input$type
+    year <- input$years
+    
+    if(!identical(type, "All"))
+    {
+      records %>% filter(Occurence_type == type) -> df
+    }
+    
+    else if(identical(type, "All")){
+      records -> df
+    }
+    # 
+    # if(identical(year, "2000-2010")){
+    #  recors %>% filter()
+    
+    # }
+    # else if(identical(year, "2000-2010")){
+    #   
+    # }
+    
+  })
+  
+  elephantIcon <- makeIcon(
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/1/1f/Asian_Elephant_Icon.svg",
-<<<<<<< HEAD
-    iconWidth = 40, iconHeight = 40,
+    iconWidth = 15, iconHeight = 15,
     iconAnchorX = 0, iconAnchorY = 0,
     shadowUrl = "https://upload.wikimedia.org/wikipedia/commons/1/1f/Asian_Elephant_Icon.svg",
-    shadowWidth = 40, shadowHeight = 40,
-=======
-    iconWidth = 38, iconHeight = 38,
-    iconAnchorX = 0, iconAnchorY = 0,
-    shadowUrl = "https://upload.wikimedia.org/wikipedia/commons/1/1f/Asian_Elephant_Icon.svg",
-    shadowWidth = 38, shadowHeight = 38,
->>>>>>> 62ba5e71c83d626d50ef1faf33ccfa96a54f7298
+    shadowWidth = 15, shadowHeight = 15,
     shadowAnchorX = 0, shadowAnchorY = 0
   )
-
+  
   output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-        options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = points, popup = records$Title, icon = greenLeafIcon)
+    
+    ele_df <- NULL
+    ele_df <- ele_filter()
+    
+    points <- cbind(ele_df$Long, ele_df$Lat)
+    
+    leaflet(states)%>%
+      addTiles(group = "OSM (default)") %>%
+      addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+      addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
+      addMarkers(data = points, popup = records$Title, icon = elephantIcon, label = records$Title)%>%
+      addPolygons(color = "green", weight = 2, 
+                  highlightOptions = highlightOptions(color = "blue", weight = 2,
+                                                      bringToFront = TRUE))
   })
 }
 
